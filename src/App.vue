16b1824b30225bacb906e3f50deb732f39ea51d4
Stephen <stephen@behnfeldt.pro>
2023-06-03 12:49:48 -0400

    Added sidebar; added list and list items to the sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
     <v-app>
         <!-- sidebar -->
+        <SideBar/>
 
         <v-main>
-        <!-- cards-->
+            <!-- cards-->
         </v-main>
 
     </v-app>
 </template>
 
+<script setup>
+import SideBar from "@/components/SideBar.vue";
+</script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-navigation-drawer expand-on-hover rail>
+        <v-list>
+            <v-list-item
+                    prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+                    title="Sandra Adams"
+                    subtitle="sandra_a88@gmail.com"
+            >
+            </v-list-item>
+            <v-divider/>
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-image" title="Color" value="color"></v-list-item>
+                <v-list-item prepend-icon="mdi-dialpad" title="Grayscale" value="gray"></v-list-item>
+            </v-list>
+        </v-list>
+
+    </v-navigation-drawer>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
