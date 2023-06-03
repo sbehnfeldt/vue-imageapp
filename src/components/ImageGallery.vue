af7c22c1e8ad550d50fad42e45ff5213b11edc32
Stephen <stephen@behnfeldt.pro>
2023-06-03 19:18:57 -0400

    Added styling

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,23 +1,26 @@
 <template>
-    <v-row>
-        <v-col v-for="n in 200" cols="6" xs="2" sm="4" md="3" lg="2" xl="1"  >
+    <v-card class="mx-5 my-2 pa-3">
+        <v-row>
+            <v-col v-for="n in 200" cols="12" xs="4" sm="3" md="2" lg="1">
 
-            <v-card>
-                <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
-                       :lazy-src="`https://picsum.photos/10/6/?image=${n * 5 + 10}`"
-                       aspect-ratio="1"
-                       cover
-                >
-                    <template v-slot:placeholder>
-                        <v-row class="fill-height ma-0" align="center" justify="center">
-                            <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
-                        </v-row>
-                    </template>
+                <v-card>
+                    <v-img :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                           :lazy-src="`https://picsum.photos/10/6/?image=${n * 5 + 10}`"
+                           aspect-ratio="1"
+                           cover
+                    >
+                        <template v-slot:placeholder>
+                            <v-row class="fill-height ma-0" align="center" justify="center">
+                                <v-progress-circular indeterminate color="grey-lighten-5"></v-progress-circular>
+                            </v-row>
+                        </template>
 
-                </v-img>
-            </v-card>
-        </v-col>
-    </v-row>
+                    </v-img>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-card>
+
 </template>
 
 <script setup>
