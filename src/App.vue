878f9bf0a4551b24d0723b46fd71a073aa6144ef
Stephen <stephen@behnfeldt.pro>
2023-06-03 09:17:22 -0400

    Remove boilerplate project files.  Remove ESLint npm dependencies.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,4 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  <div></div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-}
-</script>
